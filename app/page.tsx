dce8a3a58c36aba54859ff0f34af4ea732940610
Mari <leitzkemariana@gmail.com>
2026-06-27 20:31:38 -0300

    Refactor Home component layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Menu from "./components/menu";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col flex-1 font-sans bg-black">
       <Menu/> 
-      <main className="flex justify-center w-full max-w-3xl flex-col justify-center p-10 bg-white dark:bg-black">
-        <h1 className="text-4xl md:text-5xl font-bold tracking-tight text-gray-800 leading-tight">Planetarium</h1>
-        <h2>Guia dos planetas do sistema</h2>
+      <main className="p-10">
+        <h1 className="text-4xl md:text-5xl font-bold tracking-tight text-gray-800 leading-tight text-center">Planetarium</h1>
+        <h2 className="text-center text-gray-200">Guia dos planetas do sistema</h2>
       </main>
     </div>
   );
